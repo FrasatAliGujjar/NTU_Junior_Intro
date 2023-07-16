f95101e2c319577036e989a4393c3274a2038c48
Frasat ALi Gujjar <127954439+FrasatAliGujjar@users.noreply.github.com>
2023-07-15 20:55:52 -0700

    Version 1
    Version 1

---
+++ script.js
@@ -0,0 +1,58 @@
+function fawhatsapp() {
+
+    let url = "http://wa.me/923419385624?text="
+
+        + "*Asslam-o-Alaikum !*" + "%0a"
+        + " " + "%0a"
+        + "*Sir ! Frasat Ali Gujjar*" + "%0a"
+        + " " + "%0a"
+        + "*This is my intro*" + "%0a"
+        + "---------------------" + "%0a"
+        + " " + "%0a"
+        + "*Name :*" + " " + document.getElementById('name').value + "%0a"
+        + "*Cast :*" + " " + document.getElementById('cast').value + "%0a"
+        + "*Batch :*" + " " + document.getElementById('batch').value + "%0a"
+        + "*Class :*" + " " + document.getElementById('class').value + "%0a"
+        + "*Department :*" + " " + document.getElementById('dp').value + "%0a"
+        + "*Area :*" + " " + document.getElementById('area').value + "%0a"
+        + " " + "%0a"
+        + "---------------------" + "%0a"
+        + " " + "%0a"
+        + "*Contact Titile*" + "%0a"
+        + document.getElementById('batch').value + "-" + document.getElementById('name').value + "-" + document.getElementById('cast').value + "-" + "(" + document.getElementById('class').value + ")" + "-" + document.getElementById('area').value + "%0a"
+
+
+    window.open(url, "_blank");
+
+}
+
+
+// Aun 
+
+function mawhatsapp() {
+
+    let url = "http://wa.me/923001467962?text="
+
+        + "*Asslam-o-Alaikum !*" + "%0a"
+        + " " + "%0a"
+        + "*Sir ! Muhammad Aun*" + "%0a"
+        + " " + "%0a"
+        + "*This is my intro*" + "%0a"
+        + "---------------------" + "%0a"
+        + " " + "%0a"
+        + "*Name :*" + " " + document.getElementById('name').value + "%0a"
+        + "*Cast :*" + " " + document.getElementById('cast').value + "%0a"
+        + "*Batch :*" + " " + document.getElementById('batch').value + "%0a"
+        + "*Class :*" + " " + document.getElementById('class').value + "%0a"
+        + "*Department :*" + " " + document.getElementById('dp').value + "%0a"
+        + "*Area :*" + " " + document.getElementById('area').value + "%0a"
+        + " " + "%0a"
+        + "---------------------" + "%0a"
+        + " " + "%0a"
+        + "*Contact Titile*" + "%0a"
+        + document.getElementById('batch').value + "-" + document.getElementById('name').value + "-" + document.getElementById('cast').value + "-" + "(" + document.getElementById('class').value + ")" + "-" + document.getElementById('area').value + "%0a"
+
+
+    window.open(url, "_blank");
+
+}
